f94bd4819c3955d00197999f810ba17a669da248
Ajkuna Seipi <ajkuna.seipi@epfl.ch>
2024-06-11 19:28:14 +0200

    Rename to old notebooks

--- src/prediction/run_experiments.sh
+++ src/prediction/run_experiments.sh
@@ -6,9 +6,9 @@
 #python experiment_runner.py "correlation" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm_only_l1_l2.csv" "results_experiment_correlation-3.json"
 
 # success experiments
-#python experiment_runner.py "success" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm.csv" "results_experiment_success-1.json"
-#python experiment_runner.py "success" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm_123.csv" "results_experiment_success-2.json" 
-#python experiment_runner.py "success" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm_only_l1_l2.csv" "results_experiment_success-3.json"
+python experiment_runner.py "success" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm.csv" "results_experiment_success-1.json"
+python experiment_runner.py "success" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm_123.csv" "results_experiment_success-2.json" 
+python experiment_runner.py "success" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm_only_l1_l2.csv" "results_experiment_success-3.json"
 
 # truncation methods 
 python experiment_runner.py "truncation" "/Users/ajkunaseipi/Documents/MA4/PISA/data/robotarm.csv" "results_experiment_trunc-1.json"
